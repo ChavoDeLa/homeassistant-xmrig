04b482286eb62efbe3e5be833853ff5c49fa04b2
ChavoDeLa <116581782+ChavoDeLa@users.noreply.github.com>
2025-02-23 20:00:07 -0500

    Update summary_controller.py

--- custom_components/xmrig/summary_controller.py
+++ custom_components/xmrig/summary_controller.py
@@ -13,12 +13,36 @@ from homeassistant.core import callback
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.dispatcher import async_dispatcher_send
 from homeassistant.helpers.event import async_track_time_interval
+from homeassistant.components.sensor import SensorEntity
 
 from .const import DOMAIN, CONF_ADDRESS, CONF_TOKEN
 from .hwm_controller import HwmController
 
 _LOGGER = logging.getLogger(__name__)
 
+class XmrigConfigSensor(SensorEntity):
+    """Sensor to store full XMRig config."""
+
+    def __init__(self, controller):
+        """Initialize the sensor."""
+        self.controller = controller
+        self._attr_name = "XMRig Config"
+        self._attr_unique_id = "xmrig_config"
+
+    @property
+    def native_value(self):
+        """Return a placeholder string (config stored as attributes)."""
+        return "Config Data"
+
+    @property
+    def extra_state_attributes(self):
+        """Return full config data as attributes."""
+        return self.controller.data.get("config", {})
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up the config sensor."""
+    controller = hass.data["xmrig"]["controller"][entry.entry_id]
+    async_add_entities([XmrigConfigSensor(controller)], update_before_add=True)
 
 class SummaryController(HwmController):
     """XMRIG summary controller class"""
